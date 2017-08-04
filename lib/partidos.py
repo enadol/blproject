1b2e44aa528e86fb5cce7a88fb6d92ce6705a2a8
Enrique Lopez Magallon <enadol@yahoo.com>
2017-08-04 18:24:08 +0200

    Update partidos.py

--- lib/partidos.py
+++ lib/partidos.py
@@ -1,9 +1,9 @@
-﻿from input import jornada
+from input import jornada
 import urllib
 import json
 import sqlite3
 
-url="../2016-2017/bl.json"
+url="../2017-2018/bl.json"
 uh=urllib.urlopen(url)
 data=uh.read()
 js=json.loads(data)
@@ -106,7 +106,7 @@ else:
 		for fecha in range(0,jornada):
 			jornadascompletas=js['rounds'][fecha]['name']
 
-			conn = sqlite3.connect('../tabla16.sqlite')
+			conn = sqlite3.connect('../tabla17.sqlite')
 			cur = conn.cursor()
 			conn.text_factory = str
 			start = 0
