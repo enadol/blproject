bca9c3601632021a1bc504a4c2b105491a09313a
Enrique Lopez Magallon <enadol@yahoo.com>
2019-04-17 19:47:51 +0200

    Add files via upload

---
+++ lib/jsonformat.py
@@ -0,0 +1,9 @@
+import json
+from collections import OrderedDict
+
+#para cambiar el formato del json con indents y crear el nuevo file reformateado
+fh=json.loads(open('../2018-2019/bundesliga.json', 'r').read(), object_pairs_hook=OrderedDict)
+fh2=json.dumps(fh, indent=4, sort_keys=True)
+with open('../2018-2019/bl.json', 'w') as outfile:
+    outfile.write(fh2)
+    outfile.close()
