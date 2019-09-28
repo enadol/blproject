fb420a57a29d49dfcfc7c36f4f81bbb1d9e3bf1a
Enrique Lopez Magallon <enadol@yahoo.com>
2019-09-28 09:41:45 +0200

    Add files via upload

--- lib/puntos.py
+++ lib/puntos.py
@@ -11,25 +11,21 @@ with open(url) as json_roh:
 	
 
 
-i=0
-ganador=None
-perdedor=None
-puntoslocal=0
-puntosvisitante=0
-sumavisitante=0
-puntos=0
-sumalocal=0
-equipos=[]
-jornadas=str(jornada)
-start=0
+i = 0
+puntoslocal = 0
+puntosvisitante = 0
+sumavisitante = 0
+puntos = 0
+equipos = []
+jornadas = str(jornada)
+start = 0
 
 def getEquipos():
-	for fecha in range(0,jornada):
-		#jornadascompletas=js['rounds'][fecha]['name']
-		
-		for i in range(0,9):
-			equipo=js['rounds'][fecha]['matches'][i]['team1']['name']
-			contrario=js['rounds'][fecha]['matches'][i]['team2']['name']
+	for fecha in range(0, jornada):
+	
+		for i in range(0, 9):
+			equipo = js['rounds'][fecha]['matches'][i]['team1']['name']
+			contrario = js['rounds'][fecha]['matches'][i]['team2']['name']
 			if equipo not in equipos:
 				equipos.append(equipo)
 			if contrario not in equipos:
@@ -39,45 +35,39 @@ def getEquipos():
 
 
 def getPuntos(equipo, afavor, encontra):
-	puntos=0
+	puntos = 0
 	
 	if afavor > encontra:
-		puntos=3
-	elif afavor==encontra:
-		puntos=1
+		puntos = 3
+	elif afavor == encontra:
+		puntos = 1
 	else:
-		puntos=0
+		puntos = 0
 	
 	return puntos
 
 def getPuntosAcumulados(puntoslocal, puntos):
-	puntoslocal=puntoslocal+puntos
+	puntoslocal = puntoslocal + puntos
 	return puntoslocal
 	
 
 print("Loading puntos.py...")
-# jornadainput=raw_input("Ingrese la jornada: ")
-# jornada=int(jornadainput)-1
 
 
-if jornada <0 or jornada >=35:
+if jornada < 0 or jornada >= 35:
 	print("No se jugó la jornada "+str(jornada)+" en ese torneo. Verifique y vuelva a ingresar.")
-	jornada=None
+	jornada = None
 else:
-	clubes=getEquipos()
+	clubes = getEquipos()
 
 	for club in clubes:
-		subtotaluno=0
-		subtotaldos=0
-		subtotala=0
-		subtotalb=0
-		sumbtotala=0
-		sumtotalb=0
-		sumapuntos=0
-		sumtotallocal=0
-		sumtotalvisitante=0
+		subtotaluno = 0
+		subtotaldos = 0
+		sumapuntos = 0
+		sumtotallocal = 0
+		sumtotalvisitante = 0
 
-		for fecha in range(0,jornada):
+		for fecha in range(0, jornada):
 			#jornadascompletas=js['rounds'][fecha]['name']
 			conn = sqlite3.connect('../tabla19.sqlite')
 			cur = conn.cursor()
@@ -88,33 +78,32 @@ else:
 
 				
 			for i in range(0,9):
-				equipo=js['rounds'][fecha]['matches'][i]['team1']['name']
-				afavor=js['rounds'][fecha]['matches'][i]['score1']
-				contrario=js['rounds'][fecha]['matches'][i]['team2']['name']
-				encontra=js['rounds'][fecha]['matches'][i]['score2']
+				equipo = js['rounds'][fecha]['matches'][i]['team1']['name']
+				afavor = js['rounds'][fecha]['matches'][i]['score1']
+				contrario = js['rounds'][fecha]['matches'][i]['team2']['name']
+				encontra = js['rounds'][fecha]['matches'][i]['score2']
 
-				if equipo==club:
-					puntosuno=getPuntos(equipo, afavor, encontra)
+				if equipo == club:
+					puntosuno = getPuntos(equipo, afavor, encontra)
 				
 
-					subtotaluno=getPuntosAcumulados(subtotaluno, puntosuno)
+					subtotaluno = getPuntosAcumulados(subtotaluno, puntosuno)
 					
 		
 
-				if contrario==club:
-					puntosdos=getPuntos(contrario, encontra, afavor)
+				if contrario == club:
+					puntosdos = getPuntos(contrario, encontra, afavor)
 						
 		
-					subtotaldos=getPuntosAcumulados(subtotaldos, puntosdos)
+					subtotaldos = getPuntosAcumulados(subtotaldos, puntosdos)
 					
-				sumtotallocal=subtotaluno+subtotaldos
 				
-				sumapuntos=sumtotallocal+sumtotalvisitante
-			start =fecha+1
+				sumapuntos = subtotaluno + subtotaldos
+			start = fecha+1
 			print("El equipo "+club+" sumaba "+str(subtotaluno)+" puntos como local a la "+str(start))
 			print("El equipo "+club+" sumaba "+str(subtotaldos)+" puntos como visitante a la "+str(start)	)
 			print("El equipo "+club+" sumaba "+str(sumapuntos)+" puntos en total a la "+str(start))
-			cur.execute('''INSERT OR IGNORE INTO Puntos (Equipo, Jornada, Puntos_Local, Puntos_Visitante, Total_Puntos)  VALUES (?, ?, ?, ?, ? )''', (club, fecha+1, subtotaluno, subtotaldos, sumapuntos))
+			cur.execute('''INSERT OR IGNORE INTO Puntos (Equipo, Jornada, Puntos_Local, Puntos_Visitante, Total_Puntos)  VALUES (?, ?, ?, ?, ? )''', (club, start, subtotaluno, subtotaldos, sumapuntos))
 			conn.commit()			
 	
 cur.close()
